934c2a8639a14f823e46ceb676a5d7b99ca79fa1
Netsanet Ergetie <>
2022-08-15 01:05:19 +0300

    Remove unused import

--- city_bike_app.py
+++ city_bike_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 import pandas as pd
 
 app = Flask(__name__)
@@ -12,9 +12,9 @@ def convert_second_to_minutes(seconds):
         return round(float(seconds)/60, 2)
 
 def getJourny(journey_df):
-   # For each journy return
-   # "Departure station name"
-    #"Return station name"
+    # For each journy return
+    # "Departure station name"
+    # "Return station name"
     #"Covered distance (m)" in km
     # "Duration (sec.)" in minutes
     journey_df['Covered distance (km)'] = journey_df.apply(lambda row: convert_meter_to_km(row['Covered distance (m)']), axis=1)
@@ -30,6 +30,7 @@ def getData():
         fields = ["Departure station name","Return station name","Covered distance (m)", "Duration (sec.)"]
 
         iter_csv = pd.read_csv('data/citybikes/2021-05.csv', iterator=True, nrows=1000, usecols=fields, chunksize=100)
+       
         # Filter the data set so only jonrines with  duration > 10 sec and distance > 10 meter are loaded to the memory
         journey_df = pd.concat([chunk[(chunk['Duration (sec.)'] > 10 ) & (chunk['Covered distance (m)'] > 10)] for chunk in iter_csv])
                 
